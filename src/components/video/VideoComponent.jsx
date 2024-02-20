528d4518656f100574ffcb2d7971907fc4c517b5
Rai_delgado <alexander.delgado@kushkipagos.com>
2024-02-20 12:27:34 -0500

    Implement react and video

---
+++ src/components/video/VideoComponent.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import YouTube from "react-youtube";
+
+const VideoComponent = ({
+  height,
+  width,
+  src,
+  title,
+  video_id,
+  video_height,
+  video_width,
+}) => {
+  const [play, setPlay] = useState(false);
+  const videoOptions = {
+    borderRadius: "16px",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <>
+     <div className="youtube mx-auto text-center">
+          <YouTube
+            videoId={video_id}
+            opts={videoOptions}
+            iframeClassName={`  aspect-video ${video_height} ${video_width} max-w-full bg-transparent rounded-2xl`}
+          />
+        </div>
+    </>
+  );
+};
+
+export default VideoComponent;
