2c49b3f9fbff5e0c26a7d5d9ab4185cbffccdebc
Rai_delgado <alexander.delgado@kushkipagos.com>
2024-02-20 14:13:13 -0500

    Implement welcome vide

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(), react()],
+  integrations: [tailwind(), mdx(), sitemap()],
 });
